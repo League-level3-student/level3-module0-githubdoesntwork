8107b410a5e33e775d4eab424c4e007fb922b2c0
yeetyeet <pojaluci@easy-apps.info>
2019-06-21 09:00:36 -0700

    ROBOT:godmode:

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -7,24 +7,46 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
-	
-		//2. create an array of 5 robots.
+	// 1. make a main method
+	public static void main(String[] args) {
+		int winnerNum;
+		boolean reachedEnd = false;
+		Random r = new Random();
+		Robot[] robots = new Robot[5];
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+			robots[i].setX((i * 170) + 100);
+			robots[i].setY(530);
+			robots[i].setSpeed(100);
+		}
+		while (!reachedEnd) {
+			for (int i = 0; i < robots.length; i++) {
+				robots[i].move(r.nextInt(15));
+				if (robots[i].getY() < 0) {
+					reachedEnd = true;
+					winnerNum=i+=1;
+					System.out.println("Robot #"+winnerNum+" wins! Party time.");
+					break;
+				}
+			}
+		}
+	}
+	// 2. create an array of 5 robots.
 
-		//3. use a for loop to initialize the robots.
+	// 3. use a for loop to initialize the robots.
 
-			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
-		//5. use another for loop to iterate through the array and make each robot move 
-	    //   a random amount less than 50.
-    	
-		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+	// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-		//7. declare that robot the winner and throw it a party!
-    	
-		//8. try different races with different amounts of robots.
-    	
-	    //9. make the robots race around a circular track.
+	// 5. use another for loop to iterate through the array and make each robot move
+	// a random amount less than 50.
 
-	
+	// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+	// screen.
+
+	// 7. declare that robot the winner and throw it a party!
+
+	// 8. try different races with different amounts of robots.
+
+	// 9. make the robots race around a circular track.
+
 }
