e3426637b49e87cc52b0c509f0b2316bf8731c0e
yeetyeet <pojaluci@easy-apps.info>
2019-06-22 08:22:50 -0700

    thng

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,25 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int sum=0;
+		for (int i = 0; i < values.length; i++) {
+			sum=values[i]+sum;
+		}
+		return sum;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		int average=0;
+		int sum=0;
+		for (int i = 0; i < values.length; i++) {
+			sum=values[i]+sum;
+			if(i==values.length) {
+				average=sum/i;
+			}
+		}
+		return average;
 	}
 	
 	
@@ -20,15 +30,25 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
-		return false;
+		boolean bOOlean=false;
+	for (int i : array) {
+		if(i==value) {
+			bOOlean=true;
+		}
+	}
+		return bOOlean;
 	}
 	
 	//4. Complete the method so that it returns the index of the,
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
-		
-		return 0;
+		int instance=-1;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]==value) {
+				instance=i;
+			}
+		}
+		return instance;
 	}
 }
