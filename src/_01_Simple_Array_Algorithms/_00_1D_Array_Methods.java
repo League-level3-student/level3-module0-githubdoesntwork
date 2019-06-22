8f8af740409802485157a14d1cad5154e308952b
yeetyeet <pojaluci@easy-apps.info>
2019-06-22 10:09:24 -0700

    finished module 0

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -1,54 +1,48 @@
 package _01_Simple_Array_Algorithms;
 
+import java.util.Arrays;
+
 public class _00_1D_Array_Methods {
-	//1. Complete the method so that it returns the sum of all
-	//   of the integers in the array being passed in
+	// 1. Complete the method so that it returns the sum of all
+	// of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-		int sum=0;
-		for (int i = 0; i < values.length; i++) {
-			sum=values[i]+sum;
-		}
-		return sum;
+		return Arrays.stream(values).sum();
 	}
-	
-	//2. Complete the method so that it returns the average of all
-	//   of the integers in the array being passed in
+
+	// 2. Complete the method so that it returns the average of all
+	// of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-		int average=0;
-		int sum=0;
-		for (int i = 0; i < values.length; i++) {
-			sum=values[i]+sum;
-			if(i==values.length) {
-				average=sum/i;
-			}
-		}
-		return average;
+			return Arrays.stream(values).average().orElse(Double.NaN);
 	}
-	
-	
-	//3. Complete the method so that it returns true if the integer
-	//   array contains the value specified by the second parameter.
-	//   It should otherwise return false.
+	// 3. Complete the method so that it returns true if the integer
+	// array contains the value specified by the second parameter.
+	// It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-		boolean bOOlean=false;
-	for (int i : array) {
-		if(i==value) {
-			bOOlean=true;
-		}
+		boolean result = false;
+
+        for(int i : array){
+            if(i == value){
+                result = true;
+                break;
+            }
+        }
+
+        return result;
 	}
-		return bOOlean;
-	}
-	
-	//4. Complete the method so that it returns the index of the,
-	//   first instance that the specified value occurs in the array.
-	//   If the array does not contain the specified value, it should return -1.
+
+	// 4. Complete the method so that it returns the index of the,
+	// first instance that the specified value occurs in the array.
+	// If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
-		int instance=-1;
-		for (int i = 0; i < arr.length; i++) {
-			if(arr[i]==value) {
-				instance=i;
-			}
-		}
-		return instance;
+        int i = 0; 
+        while (i < arr.length) {
+            if (arr[i] == value) { 
+                return i; 
+            } 
+            else { 
+                i = i + 1; 
+            } 
+        } 
+        return -1; 
+    } 
 	}
-}
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,26 +1,63 @@
 package _03_More_Array_Fun;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MoreArrayFun {
-	//1. Create a main method to test the other methods you write.
-	
-	
-	
-	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
-	
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in reverse order.
-	
-	
-	
-	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
-	
-	//5. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in a completely random order. Almost every run of the program should result in a different order.
-	
-	
+	// 1. Create a main method to test the other methods you write.
+	public static void main(String[] args) {
+		Random r = new Random();
+		String[] strings = { "a", "b", "c", "d", "e", "f", "g" };
+		// printAllStrings(strings);
+		// reversePrintStrings(strings);
+		// printEveryOtherString(strings);
+		printRandomString(strings);
+	}
+
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+	public static void printAllStrings(String[] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+	public static void reversePrintStrings(String[] a) {
+		for (int i = a.length - 1; i > -1; i--) {
+			System.out.println(a[i]);
+		}
+	}
+
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	public static void printEveryOtherString(String[] a) {
+		for (int i = 0; i < a.length; i += 2) {
+			System.out.println(a[i]);
+		}
+	}
+
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+	public static void printRandomString(String[] a) {
+		shuffleArray(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+	public static void shuffleArray(String[] ar) {
+		Random rnd = ThreadLocalRandom.current();
+		for (int i = ar.length - 1; i > 0; i--) {
+			int index = rnd.nextInt(i + 1);
+			String a = ar[index];
+			ar[index] = ar[i];
+			ar[i] = a;
+		}
+	}
+
 }
